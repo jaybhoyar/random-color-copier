613e0dce4ca2397987f5d566e86495a1872d27c1
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-01-03 00:04:41 +0530

    click color copy

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function randomColor() {
 	let words = "ABCDEF0123456789".split("");
 	let color = "#";
 	for (i = 0; i < 6; i++) {
-		color += words[Math.floor(Math.random() * 15)];
+		color += words[Math.floor(Math.random() * 16)];
 	}
 	return color;
 }
@@ -23,9 +23,9 @@ function makedivRows() {
 		all[i].innerHTML = colors;
 		all[i].onmouseover = function logMouseOver() {
 			all[i].style.textShadow = "-1px 0px 5px rgba(0,0,0,0.46)";
-			all[i].style.boxShadow =
-				" 0px 0px 8px 4px rgba(230, 230, 230,0.5)";
+			all[i].style.boxShadow = " 0px 0px 8px 4px rgba(230, 230, 230,0.5)";
 			all[i].style.cursor = "pointer";
+			/* Select the text field */
 		};
 		all[i].onmouseout = function logMouseOver() {
 			all[i].style.textShadow = "";
@@ -33,4 +33,14 @@ function makedivRows() {
 			all[i].style.cursor = "";
 		};
 	}
+}
+function copyCode() {
+	/* Select the text field */
+	var copyText = document.getElementById("div1");
+
+	/* Select the text field */
+	copyText.innerHTML.select();
+
+	/* Copy the text inside the text field */
+	document.execCommand("copy");
 }
